a6d9ba0f84c58615bd56d513ce38cd717f0641e4
nikhilrajpoot <nikhilrajpoot1439@gmail.com>
2026-05-01 12:00:27 +0545

    variable

---
+++ 01_Variable.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+    int i=10;
+    int j=i;
+    printf("The value of i is %d and value of j is %d" ,i ,j);
+return 0;
+}
---
+++ 02_arithematic_operator.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+    int a=45;
+    int b=4;
+    int c= a+b;
+    int d=a%b;
+     printf("The value of a is %d and value of is %d and value of c is %d and value of d is %d" ,a,b,c,d);
+return 0;
+}
