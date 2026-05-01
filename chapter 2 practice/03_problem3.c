97b148d300cfaddad28ad7ec07f380f7d2e91a32
nikhilrajpoot <nikhilrajpoot1439@gmail.com>
2026-05-01 20:28:25 +0545

    chapter 3

---
+++ chapter 2 practice/03_problem3.c
@@ -0,0 +1,12 @@
+// Check weather a number is divisible by 97 or Not.
+
+
+#include<stdio.h>
+
+int main(){
+    int a;
+    printf("Enter a number ");
+    scanf("%d" , &a);
+printf("The value of a%97 is %d", a%97);
+return 0;
+}
