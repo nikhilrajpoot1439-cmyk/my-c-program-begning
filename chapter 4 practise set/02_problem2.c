31ad298fe3d9e6f97edc5fc738dc2840d2c9fdc6
nikhilrajpoot <nikhilrajpoot1439@gmail.com>
2026-05-09 21:18:03 +0545

    problem2

---
+++ chapter 4 practise set/02_problem2.c
@@ -0,0 +1,20 @@
+
+
+//Write a program to print multiplication table of n in reverse order
+
+
+
+
+#include<stdio.h>
+
+int main(){
+   int n;
+   printf("Enter the value of n\n");
+   scanf("%d" , &n) ;
+   for (int i = 10; i ; i--)
+   {
+    printf("%d X %d = %d\n" , n ,i , n*i);
+   }
+   
+return 0;
+}
