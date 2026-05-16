3d036cbf0b7ae9d5da675007122bec918634e5a7
nikhilrajpoot <nikhilrajpoot1439@gmail.com>
2026-05-16 21:59:10 +0545

    recursion

---
+++ quiz/05_recursion.c
@@ -0,0 +1,25 @@
+
+
+// A program to calculate factorial using recursion .
+
+
+
+#include<stdio.h>
+
+int factorial(int);
+
+int factorial(int n){
+    if (n==0 || n==1)       // Base condition
+    {
+        return 1;
+    }
+    
+
+    return factorial(n-1)*n;
+}
+int main()
+{
+int a = 5;
+printf("The factorial of %d is %d" , a,factorial(a));
+return 0;
+}
