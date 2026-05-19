64d32cba5d0efce1dce5697ee876ba77eb511077
nikhilrajpoot <nikhilrajpoot1439@gmail.com>
2026-05-19 23:19:26 +0545

    for

---
+++ chapter 5/chapter 5 practise set/07_problem7.c
@@ -0,0 +1,24 @@
+
+
+// Write a program using function to print following pattern
+
+// *
+// * * *
+// * * * * *
+
+
+#include<stdio.h>
+int main()
+{
+int n =7;
+for (int i = 0; i < 7; i++)
+{
+    for (int j = 0; j < 2*i+1; j++)
+    {
+        printf("Nikhil");
+    }
+    printf("\n");
+}
+
+return 0;
+}
