ae0374d1abf42813880c02a8ea885fd00b1d3fbc
nikhilrajpoot <nikhilrajpoot1439@gmail.com>
2026-05-23 21:43:51 +0545

    problem 3

---
+++ chapter 6 prctice set/03_problem3.c
@@ -0,0 +1,20 @@
+
+//WAP to change the value of variable to ten times of its current value
+
+
+#include<stdio.h>
+void change_to_thirty_times(int* );
+
+void change_to_thirty_times(int* a){
+*a = *a * 30;
+}
+
+int main()
+{
+int x =45;
+printf("The value of x is %d\n" , x);
+change_to_thirty_times(&x);
+printf("The value of x is %d\n" , x);
+
+return 0;
+}
