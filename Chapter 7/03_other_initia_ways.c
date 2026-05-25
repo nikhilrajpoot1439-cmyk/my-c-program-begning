85e48989b23b3870c63ef8f01c9a13dc37816381
nikhilrajpoot <nikhilrajpoot1439@gmail.com>
2026-05-25 23:33:12 +0545

    array

---
+++ Chapter 7/03_other_initia_ways.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+int cgpa[3] = {9,8,8};
+for (int i = 0; i < 3; i++)
+{
+    printf("The value of array  at location %d is %d\n", i ,cgpa[i]);
+}
+return 0;
+}
---
+++ chapter 6 prctice set/array.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int marks [90];   // Reserves space to store 90 integers
+
+marks[0] = 65;
+marks[1] = 95;
+// We can go all the way till marks [89]
+printf("Marks 0 and Marks 1 is %d %d" , marks[0] , marks[1]);
+return 0;
+}
