8a558e3d27e2692ca1f5a76489ff070a53346677
nikhilrajpoot <nikhilrajpoot1439@gmail.com>
2026-06-09 21:55:34 +0545

    c langu

--- Chapter 8/chapter 8 practise set/03_problem3.c
+++ Chapter 8/chapter 8 practise set/03_problem3.c
@@ -5,23 +5,24 @@
 #include <stdio.h>
 // #include <string.h>
 int strlen(char str[]);
-int strlen(char str[]){
-     int i = 0 , count;
+int strlen(char str[])
+{
+    int i = 0, count;
     char c = str[i];
 
-    while (c!= '\0')
+    while (c != '\0')
     {
         c = str[i];
 
         i++;
     }
-    count = i-1;
+    count = i - 1;
     return count;
 }
 int main()
 {
     char str[] = "Nikhil";
-   
+
     printf("%d\n", strlen(str));
     return 0;
 }
