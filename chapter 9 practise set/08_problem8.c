636cf9af015195a6c0dfb0dd1cfb317112cc3945
nikhilrajpoot <nikhilrajpoot1439@gmail.com>
2026-06-21 15:54:28 +0545

    problem 8

---
+++ chapter 9 practise set/08_problem8.c
@@ -0,0 +1,19 @@
+// Create a strcture representing a bank statement of a customer . What feilds did you use and why?
+
+#include<stdio.h>
+#include<string.h>
+
+struct  bankacc
+{
+    int accNo;
+    char name[30];
+    char ifsc[12];
+    float balance;
+    
+};
+
+int main()
+{
+
+return 0;
+}
